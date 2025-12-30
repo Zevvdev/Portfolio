0d40cbe5bd5a94b874eadb2a8586e529f5f0a86d
kimjieun <archi.01239@gmail.com>
2025-12-30 14:50:56 +0900

    fix(reducer.ts): reducer type  수정

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,7 +1,10 @@
 // src/global/store/reducer.ts
 
+import type { AnyAction } from "redux";
 import type { RootState } from "./types";
 
+
+// 초기 상태 ( redux store의 기본값 - reducer 밖에 선언 )
 const initState: RootState = {
     userInfo: null,
     token: null,
@@ -11,3 +14,17 @@ const initState: RootState = {
     notiUnreadCount: 0,
     getNotiData: false
  };
+
+//  reducer 함수 ( 항상 state를 return )
+const reducer = (
+    state: RootState = initState,
+    action: AnyAction
+): RootState => {
+    switch (action.type) {
+        // 나중에 로그인/로그아웃 관련 액션 처리 추가 예정
+        default:
+            return state;
+    }
+};
+
+export default reducer;
--- src/store/types.ts
+++ src/store/types.ts
@@ -4,7 +4,7 @@
 // Redux는 모든 전역 상태를 단 하나의 큰 객체인 RootState 안에 저장한다.
 export interface RootState {
     userInfo: UserInfo | null;
-    logoutTimer: NodeJS.Timeout | null; // 자동 로그아웃 타이머의 ID를 저장
+    logoutTimer: number | null; // 자동 로그아웃 타이머의 ID를 저장
     token: string | null;
     commonCodes: CommonCodeGroupResponse | null; // 공통코드 전역 상태
     notiList: responseNotificationsType[]; // 알림 목록
